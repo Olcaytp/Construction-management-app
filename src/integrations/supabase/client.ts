a266c78bb90e57fcee83917b2a0434a1a0b5c86a
arzupt <parganarzu@gmail.com>
2026-04-20 11:17:21 +0200

    client.updated

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -14,7 +14,7 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, 
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
-    flowType: 'pkce', // Improved security for mobile
+    flowType: 'implicit',
   }
 });
 
